6afaef950a36102909bcd22215f8ae21b8a19b21
Cesar Rodriguez <cesars90_@hotmail.com>
2014-04-09 13:47:07 -0500

    Clase boton agregada; Se pinta la pantalla de menu

--- Documents/Cuarto/Videojuegos/ProyectoFinal/src/proyectofinal/Boton.java
+++ Documents/Cuarto/Videojuegos/ProyectoFinal/src/proyectofinal/Boton.java
@@ -15,10 +15,20 @@ import java.awt.Image;
 import java.awt.Toolkit;
 public class Boton extends Base{
      
+    /**
+     * Crea un objeto nuevo de tipo Boton
+     * @param posX es la posicion en x del boton
+     * @param posY es la posicion en y del boton
+     */
     public Boton (int posX, int posY) {
         super(posX, posY);    
     }
     
+    
+    /**
+     * Regresa el atributo de animacion del objeto
+     * @return atributo animacion del objeto Boton 
+     */
     public Animacion getAnima(){
         return anima;
     }
