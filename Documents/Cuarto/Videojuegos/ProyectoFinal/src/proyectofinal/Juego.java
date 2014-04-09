128fa2b6af1251761806309009a784458def1fba
maripastrana181 <mary.pastrana@gmail.com>
2014-04-09 14:53:44 -0500

    osdoiuasodi

--- Documents/Cuarto/Videojuegos/ProyectoFinal/src/proyectofinal/Juego.java
+++ Documents/Cuarto/Videojuegos/ProyectoFinal/src/proyectofinal/Juego.java
@@ -38,9 +38,26 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
     private boolean creditos; // Bandera para indicar la pantalla de creditos
     private boolean puntajes; // Bandera para indicar la pantalla de puntajes
     
-    private Image imFondoMenu;
+    //BOTONES
+    private Boton botonCreditos;
+    private Boton botonIniciar;
+    private Boton botonInstrucciones;
+    private Boton botonAjustes;
+    private Boton botonMejPuntajes;
     
-    private Boton botInstrucciones;
+    //Imagenes botones
+    private Image imagenBotonCreditos;
+    private Image imagenBotonInstrucciones;
+    private Image imagenBotonIniciar;
+    private Image imagenBotonAjustes;
+    private Image imagenBotonMejPuntajes;
+    
+    private Image imFondoMenu;
+    private Image imFondoAjustes;
+    private Image imFondoCreditos;
+    private Image imFondoPuntajes;
+     private Image imFondoInstrucciones;
+ 
     
     
     //Constructor
@@ -50,9 +67,45 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
         this.setSize(800, 600); //tamaño del jframe
         addKeyListener(this);
         addMouseListener(this);
+        imagenBotonCreditos = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Botones/botonCred.png"));
+        imagenBotonInstrucciones = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Botones/botonInst.png"));
+        imagenBotonIniciar = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Botones/botonIniciar.png"));
+        imagenBotonAjustes = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Botones/botonIniciar.png"));
+        imagenBotonMejPuntajes= Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Botones/botonIniciar.png"));
+        
+        botonCreditos = new Boton (0,0 );
+        botonCreditos.getAnima().sumaCuadro(imagenBotonCreditos, 300);
+        botonCreditos.setPosX(getWidth() / 2 - botonCreditos.getAncho() / 2);
+        botonCreditos.setPosY(getHeight() / 2);
+       
+        botonInstrucciones = new Boton (0,0 );
+        botonInstrucciones.getAnima().sumaCuadro(imagenBotonInstrucciones, 300);
+        botonInstrucciones.setPosX(getWidth() / 2 - botonInstrucciones.getAncho() / 2);
+        botonInstrucciones.setPosY(getHeight() / 2 + 50);
+        
+        botonIniciar= new Boton (0,0 );
+        botonIniciar.getAnima().sumaCuadro(imagenBotonIniciar, 300);
+        botonIniciar.setPosX(getWidth() / 2 - botonIniciar.getAncho() / 2 );
+        botonIniciar.setPosY(getHeight() / 2 + 100);
+        
+        botonAjustes= new Boton (0,0 );
+        botonAjustes.getAnima().sumaCuadro(imagenBotonAjustes, 300);
+        botonAjustes.setPosX(getWidth() / 2 - botonAjustes.getAncho() / 2);
+        botonAjustes.setPosY(getHeight() / 2 + 150);
+        
+        botonMejPuntajes = new Boton (0,0 );
+        botonMejPuntajes.getAnima().sumaCuadro(imagenBotonMejPuntajes, 300);
+        botonMejPuntajes.setPosX(getWidth() / 2 - botonMejPuntajes.getAncho() / 2);
+        botonMejPuntajes.setPosY(getHeight() / 2 + 200);
+        
+        
+        
         
         imFondoMenu = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Fondos/Menu.jpg")); // imagen de fondo del menu
-        
+        imFondoAjustes = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Fondos/ajustes.jpg"));
+        imFondoPuntajes = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Fondos/puntajes.jpg"));
+        imFondoCreditos = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Fondos/creditos.jpg"));
+        imFondoInstrucciones =  Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("Fondos/instrucciones.jpg"));
         nivel = 0;// Nivel 0 indica que todavia no inicia
         menu = true; // comenzamos en el menu
         // Las demas pantallas estan apagadas
@@ -153,9 +206,29 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
      * @param g es el <code>objeto grafico</code> usado para dibujar.
      */
     public void paint1(Graphics g) {
-       if (menu){
-           g.drawImage(imFondoMenu,0,0,this);
-       }
+        if (menu) {
+            g.drawImage(imFondoMenu, 0, 0, this);
+            g.drawImage(botonIniciar.getImagenI(), botonIniciar.getPosX(), botonIniciar.getPosY(), this);
+            g.drawImage(botonInstrucciones.getImagenI(), botonInstrucciones.getPosX(), botonInstrucciones.getPosY(), this);
+            g.drawImage(botonCreditos.getImagenI(), botonCreditos.getPosX(), botonCreditos.getPosY(), this);
+            g.drawImage(botonAjustes.getImagenI(), botonAjustes.getPosX(), botonAjustes.getPosY(), this);
+            g.drawImage(botonMejPuntajes.getImagenI(), botonMejPuntajes.getPosX(), botonMejPuntajes.getPosY(), this);
+
+        }
+        if(ajustes){
+             g.drawImage(imFondoAjustes, 0, 0, this);
+        }
+        
+        if(creditos){
+             g.drawImage(imFondoCreditos, 0, 0, this);
+        }
+        
+        if(puntajes){
+             g.drawImage(imFondoPuntajes, 0, 0, this);
+        }
+         if(instrucciones){
+             g.drawImage(imFondoInstrucciones, 0, 0, this);
+        }
     }
 
     /**
@@ -202,7 +275,36 @@ public class Juego extends JFrame implements Runnable, KeyListener, MouseListene
      * algun componente. e es el evento generado al hacer click con el mouse.
      */
     public void mouseClicked(MouseEvent e) {
+             int clickX = e.getX();
+             int clickY = e.getY();
+             
+          if (menu) {
+            if (botonIniciar.clickEnPersonaje(clickX, clickY)) {
+              nivel  = 1;
+              menu = false;
+          
+            }
+             if (botonAjustes.clickEnPersonaje(clickX, clickY)) {
+              ajustes = true;
+              menu = false;
+          
+            }
+              if (botonCreditos.clickEnPersonaje(clickX, clickY)) {
+              creditos = true;
+              menu = false;
+          
+            }
+              if (botonMejPuntajes.clickEnPersonaje(clickX, clickY)) {
+                  puntajes = true;
+                  menu = false;
 
+              }
+              if (botonInstrucciones.clickEnPersonaje(clickX, clickY)) {
+                  instrucciones = true;
+                  menu = false;
+
+              }
+        }
     }
 
     /**
